4d4d0983efc6c1c7ee350b2a5262f300a866cae7
himabindu1303 <neellapalahimabindu@gmail.com>
2024-10-10 12:48:30 +0530

    while loop and switch case

---
+++ while,switch.js
@@ -0,0 +1,75 @@
+switch("thursday"){                                                //1st
+    case "monday":
+        console.log("Today is monday");
+        break;
+    case "tuesday":
+        console.log("Today is tuesday");
+        break;
+    case "wednesday":
+        console.log("Today is wednesday");
+        break;
+    case "thursday":
+        console.log("Today is thursday");
+        break;
+    case "friday":
+        console.log("Today is friday");
+        break;
+    case "saturday":
+        console.log("Today is saturday");
+        break;
+    case "sunday":
+        console.log("Today is sunday");
+        break;
+}
+
+switch("june"){                                                 //2nd
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("Wow, Its june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "aug":
+        console.log("august");
+        break;
+    case "sep":
+        console.log("september");
+        break;
+    case "oct":
+        console.log("october");
+        break;
+    case "nov":
+        console.log("november");
+        break;
+    case "dec":
+        console.log("december");
+}
+
+var a=1;                                                       //3rd
+while(a<=10){
+    if(a%2==0){
+        console.log(a+" is even number");    
+    }
+    a++;
+}
+
+var b=10;                                                        //4th
+while(b>=1){
+    console.log(b);
+    b--;
+}
